77944c920aeb2acb18c598057fdbd81123d29620
Megan <mleavens19@gmail.com>
2022-04-07 10:42:55 -0400

    first commit

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -10,8 +10,8 @@ export default function HomePage() {
         <Header />
         <div className = 'container'>
         <Card>
-                <h1 className = 'home-h1'>Task Manager</h1>
-                <p className = 'home-para-1'>Enjoy managing your tasks
+                <h1 className = 'home-h1'>Megan Leavens</h1>
+                <p className = 'home-para-1'>
                     <Link to= "/tasklist"> here!</Link>
                 </p>
                     <p className = "home-para-2">
